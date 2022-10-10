d6e07d3405149fa05fc88dc7a6950d6f2a96f2d0
David Wang <davidw@Davids-MacBook-Pro-2.local>
2022-10-10 14:57:34 -0700

    changed design of website

--- js/scripts.js
+++ js/scripts.js
@@ -7,28 +7,60 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+const myModal = document.getElementById('myModal')
+const myInput = document.getElementById('myInput')
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            offset: 74,
-        });
-    };
+myModal.addEventListener('shown.bs.modal', () => {
+  myInput.focus()
+})
 
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+navLinks.forEach((l) => {l.addEventListener('click', () => { bsCollapse.toggle() })
+})
+// let slideIndex = 0;
+// showSlides();
 
-});
+// function showSlides() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
+
+// window.addEventListener('DOMContentLoaded', event => {
+
+//     // Activate Bootstrap scrollspy on the main nav element
+//     const sideNav = document.body.querySelector('#sideNav');
+//     if (sideNav) {
+//         new bootstrap.ScrollSpy(document.body, {
+//             target: '#sideNav',
+//             offset: 74,
+//         });
+//     };
+
+//     // Collapse responsive navbar when toggler is visible
+//     const navbarToggler = document.body.querySelector('.navbar-toggler');
+//     const responsiveNavItems = [].slice.call(
+//         document.querySelectorAll('#navbarResponsive .nav-link')
+//     );
+//     responsiveNavItems.map(function (responsiveNavItem) {
+//         responsiveNavItem.addEventListener('click', () => {
+//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
+//                 navbarToggler.click();
+//             }
+//         });
+//     });
+
+// });
+
+
+
+
