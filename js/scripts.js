06b140b8bdfc5590232f4ffe15ebb90475870761
david <david311@student.ubc.ca>
2023-06-21 10:59:50 -0700

    revert js to original

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+// /*!
+// * Start Bootstrap - Resume v7.0.4 (https://startbootstrap.com/theme/resume)
+// * Copyright 2013-2021 Start Bootstrap
+// * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+// */
+// //
+// // Scripts
+// // 
+
 /*!
 * Start Bootstrap - Resume v7.0.4 (https://startbootstrap.com/theme/resume)
 * Copyright 2013-2021 Start Bootstrap
@@ -7,63 +16,88 @@
 // Scripts
 // 
 
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
 // const myModal = document.getElementById('myModal')
 // const myInput = document.getElementById('myInput')
-// document.addEventListener('DOMContentLoaded', function() {
-//     var element = document.getElementById('myModal');
-//     if (element) {
-//     element.addEventListener('shown.bs.modal', () => {
-//         myInput.focus()
-//       })
-//   });
+
+// element.addEventListener('shown.bs.modal', () => {
+//     myInput.focus()
+//     })
 
 
-const navLinks = document.querySelectorAll('.nav-item')
-const menuToggle = document.getElementById('navbarSupportedContent')
+
+// const navLinks = document.querySelectorAll('.nav-item')
+// const menuToggle = document.getElementById('navbarResponsive')
 // const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
-navLinks.forEach((l) => {l.addEventListener('click', () => { bsCollapse.toggle() })
-})
-// let slideIndex = 0;
-// showSlides();
+// navLinks.forEach((l) => {l.addEventListener('click', () => { bsCollapse.toggle() })
+// })
+// // let slideIndex = 0;
+// // showSlides();
 
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slideIndex++;
-//   if (slideIndex > slides.length) {slideIndex = 1}
-//   slides[slideIndex-1].style.display = "block";
-//   setTimeout(showSlides, 2000); // Change image every 2 seconds
-// }
+// // function showSlides() {
+// //   let i;
+// //   let slides = document.getElementsByClassName("mySlides");
+// //   for (i = 0; i < slides.length; i++) {
+// //     slides[i].style.display = "none";
+// //   }
+// //   slideIndex++;
+// //   if (slideIndex > slides.length) {slideIndex = 1}
+// //   slides[slideIndex-1].style.display = "block";
+// //   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// // }
 
 
-// window.addEventListener('DOMContentLoaded', event => {
+// // window.addEventListener('DOMContentLoaded', event => {
 
-//     // Activate Bootstrap scrollspy on the main nav element
-//     const sideNav = document.body.querySelector('#sideNav');
-//     if (sideNav) {
-//         new bootstrap.ScrollSpy(document.body, {
-//             target: '#sideNav',
-//             offset: 74,
-//         });
-//     };
+// //     // Activate Bootstrap scrollspy on the main nav element
+// //     const sideNav = document.body.querySelector('#sideNav');
+// //     if (sideNav) {
+// //         new bootstrap.ScrollSpy(document.body, {
+// //             target: '#sideNav',
+// //             offset: 74,
+// //         });
+// //     };
 
-//     // Collapse responsive navbar when toggler is visible
-//     const navbarToggler = document.body.querySelector('.navbar-toggler');
-//     const responsiveNavItems = [].slice.call(
-//         document.querySelectorAll('#navbarResponsive .nav-link')
-//     );
-//     responsiveNavItems.map(function (responsiveNavItem) {
-//         responsiveNavItem.addEventListener('click', () => {
-//             if (window.getComputedStyle(navbarToggler).display !== 'none') {
-//                 navbarToggler.click();
-//             }
-//         });
-//     });
+// //     // Collapse responsive navbar when toggler is visible
+// //     const navbarToggler = document.body.querySelector('.navbar-toggler');
+// //     const responsiveNavItems = [].slice.call(
+// //         document.querySelectorAll('#navbarResponsive .nav-link')
+// //     );
+// //     responsiveNavItems.map(function (responsiveNavItem) {
+// //         responsiveNavItem.addEventListener('click', () => {
+// //             if (window.getComputedStyle(navbarToggler).display !== 'none') {
+// //                 navbarToggler.click();
+// //             }
+// //         });
+// //     });
 
-// });
+// // });
 
 
 
